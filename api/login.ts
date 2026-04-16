732fa8db692b90678cd562c045204b027e6b00b7
ayan506 <85383220+ayan506@users.noreply.github.com>
2026-04-16 21:44:07 +0530

    Create login.ts

---
+++ api/login.ts
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { username, password } = req.body;
+
+    if (username === "admin" && password === "mni@school2024") {
+      return res.status(200).json({
+        success: true,
+      });
+    }
+
+    return res.status(401).json({
+      success: false,
+      message: "Invalid credentials",
+    });
+  }
+
+  res.status(405).end();
+}
